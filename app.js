16ee22557e1a7a22344c99b4f9783e8d73f5e3df
tkn13 <thaksin.ckn@gmail.com>
2024-02-02 13:41:14 +0700

    v2

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-let real = document.getElementById("real");
-let gent = document.getElementById("gent");
-let dropdown = document.getElementById("Dropdown");
+const toggle_btn = document.getElementById('toggle-btn');
+const menu = document.getElementById('menu');
 
-real.addEventListener("mouseover", function () {
-    gent.classList.add("small");
-});
-
-real.addEventListener("mouseleave", function () {
-    gent.classList.remove("small");
-});
-
-gent.addEventListener("mouseover", function () {
-    real.classList.add("small");
-});
-
-gent.addEventListener("mouseleave", function () {
-    real.classList.remove("small");
-});
-
-dropdown.addEventListener("click", function () {
-    dropdown.classList.add("show");
-    console.log("show");
+toggle_btn.addEventListener('click', () => {
+    console.log('clicked');
+    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
 });
